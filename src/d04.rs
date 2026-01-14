07c5bc78f87df10b8e2e2723c7c73694d3e01c9e
Maxi Barmetler <maxi.barmetler@gmail.com>
2026-01-15 00:18:51 +0100

    d04p1

---
+++ src/d04.rs
@@ -0,0 +1,120 @@
+#![feature(array_try_from_fn)]
+
+use aoc2016::AlphabetMap;
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::array::from_fn;
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Default)]
+struct Room<'s> {
+    name: &'s str,
+    sector_id: u32,
+    checksum: [char; 5],
+}
+
+// can't use FromStr because that doesn't allow you to return a type depending on the lifetime
+// of the &str
+impl<'s> TryFrom<&'s str> for Room<'s> {
+    type Error = ();
+
+    fn try_from(s: &'s str) -> Result<Self, Self::Error> {
+        lazy_static! {
+            // language=regexp
+            static ref Pat: Regex = Regex::new(r#"([a-z\-]*?)-(\d+)\[([a-z]{5})]"#).unwrap();
+        }
+        let (_, [name, sector_id, checksum]) = Pat.captures(s).ok_or(())?.extract();
+        let mut c = checksum.chars();
+        Ok(Room {
+            name,
+            sector_id: sector_id.parse().map_err(|_| ())?,
+            // unwrap is safe here because the regex would have failed already.
+            checksum: from_fn(|_| c.next().unwrap()),
+        })
+    }
+}
+
+fn calculate_checksum(name: &str) -> [char; 5] {
+    const {
+        assert!(AlphabetMap::<usize>::COUNT >= 5);
+    }
+    let mut counts = AlphabetMap::<usize>::new();
+    for c in name.chars() {
+        if !c.is_ascii_lowercase() {
+            continue;
+        }
+        counts[c] += 1;
+    }
+    let mut entries = <[(char, usize); _]>::from(counts);
+    entries.sort_by_key(|&(c, count)| (usize::MAX - count, c));
+    let mut it = entries.into_iter().map(|(c, _)| c);
+    from_fn(|_| it.next().unwrap())
+}
+
+impl Room<'_> {
+    fn is_valid(&self) -> bool {
+        calculate_checksum(self.name) == self.checksum
+    }
+}
+
+fn main() {
+    let input = include_str!("d04.txt");
+    let sector_sum: u32 = input
+        .lines()
+        .map(Room::try_from)
+        .map(Result::unwrap)
+        .filter(Room::is_valid)
+        .map(|r| r.sector_id)
+        .sum();
+    println!("Part1: {}", sector_sum);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        assert_eq!(
+            "aaaaa-bbb-z-y-x-123[abxyz]".try_into(),
+            Ok(Room {
+                name: "aaaaa-bbb-z-y-x",
+                sector_id: 123,
+                checksum: ['a', 'b', 'x', 'y', 'z']
+            })
+        )
+    }
+
+    #[test]
+    fn test_calculate_checksum() {
+        assert_eq!(
+            calculate_checksum("aaaaa-bbb-z-y-x"),
+            ['a', 'b', 'x', 'y', 'z']
+        );
+        assert_eq!(
+            calculate_checksum("a-b-c-d-e-f-g-h"),
+            ['a', 'b', 'c', 'd', 'e']
+        );
+        assert_eq!(
+            calculate_checksum("not-a-real-room"),
+            ['o', 'a', 'r', 'e', 'l']
+        );
+        assert_eq!(
+            calculate_checksum("totally-real-room"),
+            ['l', 'o', 'a', 'r', 't']
+        );
+    }
+
+    #[test]
+    fn test_is_valid() {
+        assert_eq!(
+            [
+                "aaaaa-bbb-z-y-x-123[abxyz]",
+                "a-b-c-d-e-f-g-h-987[abcde]",
+                "not-a-real-room-404[oarel]",
+                "totally-real-room-200[decoy]",
+            ]
+            .map(|s| Room::try_from(s).unwrap().is_valid()),
+            [true, true, true, false]
+        );
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+use std::ops::{Index, IndexMut};
+use std::slice;
+
+const COUNT: usize = (b'z' - b'a') as usize + 1;
+type Entry<Value> = (char, Value);
+type Entries<Value> = [Entry<Value>; COUNT];
+
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub struct AlphabetMap<Value> {
+    entries: Entries<Value>,
+}
+
+impl<Value> AlphabetMap<Value> {
+    pub const COUNT: usize = (b'z' - b'a') as usize + 1;
+}
+
+impl<Value> IntoIterator for AlphabetMap<Value> {
+    type Item = Entry<Value>;
+    type IntoIter = <Entries<Value> as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.entries.into_iter()
+    }
+}
+
+impl<Value: Default> AlphabetMap<Value> {
+    pub fn new() -> Self {
+        let mut entries = Entries::<Value>::default();
+        for (i, entry) in entries.iter_mut().enumerate().take(COUNT) {
+            entry.0 = (b'a' + i as u8) as char
+        }
+        Self { entries }
+    }
+}
+
+impl<Value: Default> Default for AlphabetMap<Value> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<Value> AlphabetMap<Value> {
+    pub fn iter(&self) -> slice::Iter<'_, Entry<Value>> {
+        self.entries.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> slice::IterMut<'_, Entry<Value>> {
+        self.entries.iter_mut()
+    }
+}
+
+impl<Value> From<Entries<Value>> for AlphabetMap<Value> {
+    fn from(value: Entries<Value>) -> Self {
+        AlphabetMap { entries: value }
+    }
+}
+
+impl<Value> From<AlphabetMap<Value>> for Entries<Value> {
+    fn from(value: AlphabetMap<Value>) -> Self {
+        value.entries
+    }
+}
+
+impl<Value> Index<char> for AlphabetMap<Value> {
+    type Output = Value;
+
+    fn index(&self, index: char) -> &Self::Output {
+        assert!(index >= 'a');
+        assert!(index <= 'z');
+        &self.entries[(index as u8 - b'a') as usize].1
+    }
+}
+
+impl<Value> IndexMut<char> for AlphabetMap<Value> {
+    fn index_mut(&mut self, index: char) -> &mut Self::Output {
+        assert!(index >= 'a');
+        assert!(index <= 'z');
+        &mut self.entries[(index as u8 - b'a') as usize].1
+    }
+}
