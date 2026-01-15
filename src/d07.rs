36b91236ae7479f4be598f912ea8921c83e0ddba
Maxi Barmetler <maxi.barmetler@gmail.com>
2026-01-15 21:27:51 +0100

    d07p2

--- src/d07.rs
+++ src/d07.rs
@@ -1,23 +1,24 @@
 use itertools::Itertools;
+use std::collections::HashSet;
 
 fn supports_tls(ipv7_addr: &str) -> bool {
     let mut is_inside_brackets = false;
     let mut contains_abba = false;
-    for (a, b, c, d) in ipv7_addr.chars().tuple_windows() {
-        if a == '[' {
+    for (a, b, c, d) in ipv7_addr.as_bytes().iter().copied().tuple_windows() {
+        if a == b'[' {
             is_inside_brackets = true;
             continue;
-        } else if a == ']' {
+        } else if a == b']' {
             is_inside_brackets = false;
             continue;
         }
         let is_abba = a == d
             && b == c
             && a != b
-            && a.is_alphabetic()
-            && b.is_alphabetic()
-            && c.is_alphabetic()
-            && d.is_alphabetic();
+            && a.is_ascii_alphabetic()
+            && b.is_ascii_alphabetic()
+            && c.is_ascii_alphabetic()
+            && d.is_ascii_alphabetic();
         if is_inside_brackets && is_abba {
             return false;
         }
@@ -28,10 +29,41 @@ fn supports_tls(ipv7_addr: &str) -> bool {
     contains_abba
 }
 
+fn supports_ssl(ipv7_addr: &str) -> bool {
+    let mut abas = HashSet::new();
+    let mut babs = HashSet::new();
+    let mut is_inside_brackets = false;
+    for (a, b, c) in ipv7_addr.as_bytes().iter().copied().tuple_windows() {
+        if a == b'[' {
+            is_inside_brackets = true;
+            continue;
+        } else if a == b']' {
+            is_inside_brackets = false;
+            continue;
+        }
+        if a == c
+            && a != b
+            && a.is_ascii_alphabetic()
+            && b.is_ascii_alphabetic()
+            && c.is_ascii_alphabetic()
+        {
+            if is_inside_brackets {
+                babs.insert([a, b]);
+            } else {
+                abas.insert([a, b]);
+            }
+        }
+    }
+    abas.into_iter().any(|[a, b]| babs.contains(&[b, a]))
+}
+
 fn main() {
     let input = include_str!("d07.txt");
     let valid_count = input.lines().filter(|l| supports_tls(l)).count();
     println!("Part1: {}", valid_count);
+
+    let valid_count = input.lines().filter(|l| supports_ssl(l)).count();
+    println!("Part2: {}", valid_count);
 }
 
 #[cfg(test)]
@@ -44,5 +76,13 @@ mod tests {
         assert!(!supports_tls("abcd[bddb]xyyx"));
         assert!(!supports_tls("aaaa[qwer]tyui"));
         assert!(supports_tls("ioxxoj[asdfgh]zxcvbn"));
+    }
+
+    #[test]
+    fn test_supports_ssl() {
+        assert!(supports_ssl("aba[bab]xyz"));
+        assert!(!supports_ssl("xyx[xyx]xyx"));
+        assert!(supports_ssl("aaa[kek]eke"));
+        assert!(supports_ssl("zazbz[bzb]cdb"));
     }
 }
